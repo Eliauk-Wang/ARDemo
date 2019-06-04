4a1988e275d2175f1e64945d47f65292782b3e67
MGBYGIT <754764967@qq.com>
2019-06-04 17:12:08 +0800

    commit

---
+++ ARDemo/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+    public Transform target;
+    [SerializeField]
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(target.position, Vector3.up, speed);
+	
+	}
+}
